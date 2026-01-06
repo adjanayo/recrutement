0d960df3373d97d3995e52c97bbefca3faaadd8b
mbaroland <mbaroland.kumena@ifnti.com>
2026-01-06 14:23:28 +0000

    init

---
+++ recru/recru/src/test/java/com/example/recru/RecruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
